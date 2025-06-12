3d2a1b267d83085fceaf2b177ff82b0e77664ea6
shivsri1405 <shivsri1405@gmail.com>
2025-06-12 18:16:12 +0000

    [UPD] homepage add slider and featured category

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Banner
+from .models import Banner, FeaturedCategory, Slider
 # Create your views here.
 def home(request):
     bannerObjs = Banner.objects.all()
+    FeaturedCategoryObjs = FeaturedCategory.objects.all()
+    sliderObjs = Slider.objects.all()
     # return HttpResponse("Hello shop")
-    context = {"bannerObjs":bannerObjs}
-    print(bannerObjs[1:])
+    context = {"bannerObjs":bannerObjs,"FeaturedCategoryObjs":FeaturedCategoryObjs, "sliderObjs":sliderObjs}
+    # print(bannerObjs[1:])
     return render(request, "shop/home.html", context)
 
